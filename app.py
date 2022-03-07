91aa05312bdf222af6f0751174840f74571582bb
gighomework <101001805+gighomework@users.noreply.github.com>
2022-03-08 02:07:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from linebot import (
 
 app = Flask(__name__)
 
-lineaccesstoken = 'KkIIUDSiGrMEoeUi+iRiZNfY5Gce7zyINhGfZbR0ln+VcCZTDRg3E01IlU5QuERIQ7SABUzH/J6G7ReeCFlgM0xQG388iOrY4e5WKZ6m2rOR/6Twg8NeWPHAejcLnEbQnlG5uMeZC+9Vgx/ExbOp2QdB04t89/1O/w1cDnyilFU='
+lineaccesstoken = 'KkIIUDSiGrMEoeUi+iRiZNfY5Gce7zyINhGfZbR0ln+VcCZTDRg3E01IlU5QuERIQ7SABUzH/J6G7ReeCFlgM0xQG388iOrY4e5WKZ6m2rOR/6Twg8NeCnD894e5WKZ6m2rOR/6Twg8NeCnD894E5WKZ6m2rOR/6Twg8NeCnD89'
 line_bot_api = LineBotApi(lineaccesstoken)
 
 casedata = pd.read_excel('casedata.xlsx')
